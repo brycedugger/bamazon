2bacb19868a851ee182e8693ffac55dbc475aa7d
Bryce Dugger <bryce.dugger@outlook.com>
2019-11-09 23:12:28 -0800

    challege two view products and low inventory up and running.

--- Challenge2/bamazonManager.js
+++ Challenge2/bamazonManager.js
@@ -1,9 +1,3 @@
-//once the order is placed, if the store doesn't have
-//enough product, say "Insufficient quantity!"
-
-//otherwise, update the quantity in the database
-//then show the total cost of their purchase
-
 var inquirer = require("inquirer");
 var mysql = require("mysql");
 
@@ -24,62 +18,68 @@ var connection = mysql.createConnection({
 
 connection.connect(function(err) {
     if (err) throw err;
-    displayItems();
+    runManager();
 });
 
-function displayItems() {
-  var productsArray = [];
-  connection.query("SELECT * FROM products", function (err, res) {
-    if (err) throw err;
-    for (var i = 0; i < res.length; i++) {
-      productsArray.push("ID: " + res[i].item_id + "||" + "Item: " + res[i].product_name + "||" + "Size: " + res[i].size + "||" + "Price: " + res[i].price + "||" + "Quantity: " + res[i].stock_quantity);
-    }
-    console.log(productsArray);
-    customerQuery();
-  })
-};
-
-function customerQuery() {
-  inquirer
-    .prompt([
-      {
-        name: "item_id",
-        type: "input",
-        message: "Input the item ID of the product you'd like to purchase."
-      },
-      {
-        name: "quantity",
-        type: "input",
-        message: "How many would you like to purchase?"
-      }
-    ])
-    .then(function (answer) {
-      checkStockQuantity(answer.item_id, parseInt(answer.quantity));
-    });
-};
-
-
-
-//make below its own function checkstockquantity
-
-function checkStockQuantity(id, quantity) {
-  connection.query("SELECT * FROM products WHERE ?", [{ item_id: id }], function (err, results) {
-    if (err) throw err;
-    var dbQuantity = parseInt(results[0].stock_quantity);
-    if (dbQuantity > quantity) {
-      var newQuantity = dbQuantity - quantity;
-      var totalPrice = quantity * parseFloat(results[0].price);
-      // console.log("hello");
-      connection.query("UPDATE products SET ? WHERE ?", [{stock_quantity: newQuantity},{ item_id: id }], function (error) {
-          if (error) throw err;
-          console.log("Thank you for your purchase! Your total was $: " + totalPrice);
+function runManager() {
+    inquirer
+      .prompt({
+        name: "action",
+        type: "list",
+        message: "What would you like to do?",
+        choices: [
+          "View Products for Sale",
+          "View Low Inventory",
+          "Add to Inventory",
+          "Add New Product",
+          "exit"
+        ]
+      })
+      .then(function(answer) {
+        switch (answer.action) {
+        case "View Products for Sale":
+          viewProducts();
+          break;
+  
+        case "View Low Inventory":
+          lowInventory();
+          break;
+  
+        case "Add to Inventory":
+          addInventory();
+          break;
+  
+        case "Add New Product":
+          newProduct();
+          break;
+  
+        case "exit":
           connection.end();
+          break;
         }
-      );
-    }
-    else {
-      console.log("Insufficient Quanity! Try again.");
-      connection.end();
-    }
-  });
+      });
+  }
+
+function viewProducts() {
+    var productsArray = [];
+    connection.query("SELECT * FROM products", function (err, res) {
+        if (err) throw err;
+        for (var i = 0; i < res.length; i++) {
+            productsArray.push("ID: " + res[i].item_id + "||" + "Item: " + res[i].product_name + "||" + "Size: " + res[i].size + "||" + "Price: " + res[i].price + "||" + "Quantity: " + res[i].stock_quantity);
+        }
+        console.log(productsArray);
+        runManager();
+    })
+};
+
+function lowInventory() {
+    var productsArray = [];
+    connection.query("SELECT * FROM products WHERE stock_quantity < 3", function (err, res) {
+        if (err) throw err;
+        for (var i = 0; i < res.length; i++) {
+            productsArray.push("ID: " + res[i].item_id + "||" + "Item: " + res[i].product_name + "||" + "Size: " + res[i].size + "||" + "Price: " + res[i].price + "||" + "Quantity: " + res[i].stock_quantity);
+        }
+        console.log(productsArray);
+        runManager();
+    })
 };
